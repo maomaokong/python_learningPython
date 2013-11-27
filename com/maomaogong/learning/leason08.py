6d53079200d589d1a7078efd6f710347d341f81e
maomaogong <maomaokong@gmail.com>
2013-11-27 18:38:10 +0800

    20131126_v00.00.04_adding more leason

---
+++ com/maomaogong/learning/leason08.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+'''
+Created on 27 Nov 2013
+
+@author: t80006407
+
+Title: Introduction to Lists
+Adding Shebang on top for linux
+'''
+
+myFears = [
+           "clowns"
+           , "dentists"
+           , "endless, crippling lonliness"
+           , "my mailman"
+           , "5688"
+           , 1
+           ]
+
+print "myFears"
+print myFears
+print
+
+print "myFears[1]"
+print myFears[1]
+print
+
+print "len(myFears)"
+print len(myFears)
+print
+
+print "myFears[4]"
+print myFears[4]
+print
+
+print "myFears[5]"
+print myFears[5]
+print
